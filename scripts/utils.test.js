90128818f9d3d5828d2e367bd607bb52a7723a69
Quentin Roy <quentin@quentinroy.fr>
2018-02-19 22:13:53 +0100

    test: create utils tests

---
+++ scripts/utils.test.js
@@ -0,0 +1,17 @@
+const uniq = require('lodash/uniq');
+const mapValues = require('lodash/mapValues');
+const { exclusionGroupsToBlacklists } = require('./utils');
+
+test('exclusionGroupsToBlacklists', () => {
+  expect(
+    mapValues(
+      exclusionGroupsToBlacklists([['bob', 'anna', 'jack'], ['bob', 'alice']]),
+      val => uniq(val).sort(),
+    ),
+  ).toEqual({
+    bob: ['bob', 'anna', 'jack', 'alice'].sort(),
+    anna: ['bob', 'anna', 'jack'].sort(),
+    jack: ['bob', 'anna', 'jack'].sort(),
+    alice: ['bob', 'alice'].sort(),
+  });
+});
