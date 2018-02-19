83358131123e6da10a10859d521d2174eda4bd63
Quentin Roy <quentin@quentinroy.fr>
2018-02-19 17:44:32 +0100

    refactor: split functions for easier testing

--- scripts/index.js
+++ scripts/index.js
@@ -1,75 +1,7 @@
-const shuffle = require('lodash/shuffle');
-const munkres = require('munkres-js');
 const deepMerge = require('deepmerge');
-const uniq = require('array-uniq');
-
-/**
- * Create blacklists from exclusion groups: a participant cannot be the santa of
- * someone who is in his exclusion group.
- *
- * @param {Array.<string[]>} exclusionGroups exclusion groups of participants.
- * @return {Object.<string,string[]>} a dictionary whose keys are
- * participants and values a list of participants they cannot be the santa of.
- */
-const exclusionGroupsToBlackLists = exclusionGroups =>
-  exclusionGroups.reduce(
-    (res, group) =>
-      Object.assign(
-        {},
-        res,
-        group.reduce(
-          (fRes, p) =>
-            Object.assign({}, fRes, { [p]: [...group, ...(res[p] || [])] }),
-          {},
-        ),
-      ),
-    {},
-  );
-
-/**
- * Actually perform the santa generation after the options have been processed.
- *
- * @param {string[]} participants The list of all participants.
- * @param {{}[]} pastChristmases An array containing the previous attributions.
- * @param {Object.<string,string[]>} blackLists a dictionary whose keys are
- * participants and values a list of participants they cannot be the santa of.
- * @return {Object.<string,string>} The new assignations.
- */
-const generateSantas = (participants, pastChristmases, blackLists) => {
-  // Create the matrix of costs for Munkres. Columns are santas and rows
-  // are receivers. Cells are the cost of assigning the corresponding santa to
-  // the corresponding receiver. The cost of the assignation of a santa with
-  // a receiver is the number of times this santa has already been assigned to
-  // the corresponding receiver (except if the receiver is in the santa's
-  // blackList, in this case it is INFINITY).
-  const costMatrix = participants.map(santa => {
-    const santaBlackList = blackLists[santa] || [];
-    return participants.map(receiver => {
-      if (receiver === santa || santaBlackList.includes(receiver)) {
-        return Number.POSITIVE_INFINITY;
-      }
-      return pastChristmases.reduce(
-        (count, assignations) =>
-          assignations[santa] === receiver ? count + 1 : count,
-        0,
-      );
-    });
-  });
-
-  // Use the Munkres algorithm to get the assignation. Because the cost of an
-  // an assignation is the number of times this assignation already happened,
-  // Munkres will minimize the number of re-assignation.
-  const assignations = munkres(costMatrix)
-    // Convert the results of munkres to [santa, receiver] entries.
-    .map(entry => entry.map(pi => participants[pi]))
-    // Transform into a dictionary.
-    .reduce(
-      (obj, [santa, receiver]) => Object.assign({}, obj, { [santa]: receiver }),
-      {},
-    );
-
-  return assignations;
-};
+const uniq = require('lodash/uniq');
+const { generateSantas } = require('./generate-santas');
+const { exclusionGroupsToBlacklists } = require('./utils');
 
 /**
  * Generate a new secret Santa attributions based on past attributions and
@@ -88,8 +20,8 @@ const generateSantas = (participants, pastChristmases, blackLists) => {
  * @param {string[]} [options.participants] The list of all participants.
  * If omitted, then the participants appearing in exclusionGroups and
  * as a blackLists key will be used.
- * @param {Object.<string,string>[]} [options.history] An array
- * containing the previous attributions (dictionnaries whose keys are the
+ * @param {List.<Object.<string,string>>} [options.history] An array
+ * containing the previous attributions (dictionaries whose keys are the
  * santas, and values their receiver).
  * @param {Object.<string,string[]>} [options.blackLists] A dictionary whose
  * keys are participants and values a list of participants they cannot be the
@@ -101,8 +33,7 @@ const generateSantas = (participants, pastChristmases, blackLists) => {
  * the assignation algorithm. Makes the output non deterministic.
  * @return {Object.<string,string>} The new assignations.
  */
-module.exports = options => {
-  // Parse the options.
+const santasDeRoy = options => {
   const {
     history = [],
     blackLists: initBlackList = {},
@@ -114,25 +45,18 @@ module.exports = options => {
     random = true,
   } = Array.isArray(options) ? { participants: options } : options;
 
+  // Check if there is participants.
   if (!participants || participants.length <= 0) {
     throw new Error('No participants specified.');
   }
 
+  // Convert exclusion groups to blacklists and merge them with the blacklists.
   const blackLists = deepMerge(
-    exclusionGroupsToBlackLists(exclusionGroups),
+    exclusionGroupsToBlacklists(exclusionGroups),
     initBlackList,
   );
 
-  const randomizedParticipants = random ? shuffle(participants) : participants;
-  const assignations = generateSantas(
-    randomizedParticipants,
-    history,
-    blackLists,
-  );
-  if (!random) return assignations;
-  // If the participants have been randomized, re-sort them.
-  return participants.reduce(
-    (obj, p) => Object.assign({}, obj, { [p]: assignations[p] }),
-    {},
-  );
+  return generateSantas({ random, participants, history, blackLists });
 };
+
+module.exports = santasDeRoy;
